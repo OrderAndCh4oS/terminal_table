eb8e9b06bef4c5e7a2a5083ded9ac2df930e17e7
sarcoma <sarcoma@live.co.uk>
2019-03-14 00:36:37 +0000

    Columns, Rows and Headers can be coloured with ANSI colours.

--- terminal_table/terminal_table.py
+++ terminal_table/terminal_table.py
@@ -3,27 +3,49 @@ from ansi_colours import AnsiColours as Colour
 
 class Table:
     @staticmethod
-    def create(rows, headers):
+    def create(rows, headers, row_colours=None, column_colours=None, header_colours=None, header_colour=None):
+        if row_colours is None:
+            row_colours = []
+        if column_colours is None:
+            column_colours = []
+        if header_colours is None:
+            header_colours = []
         col_widths = [len(header) for header in headers]
         for row in rows:
             for i, column in enumerate(row):
                 if len(str(column)) > col_widths[i]:
                     col_widths[i] = len(str(column))
         output = ''
-        output += "%s\n" % Table.make_row(headers, col_widths)
+        output += "%s\n" % Table.make_row(
+            headers,
+            col_widths,
+            column_colours=header_colours,
+            row_colour=header_colour
+        )
         output += "%s\n" % Table.make_underline(col_widths)
-        for row in rows:
-            output += "%s\n" % Table.make_row(row, col_widths, has_key=True)
+        for i, row in enumerate(rows):
+            row_colour = row_colours[i % len(row_colours)] if len(row_colours) else None
+            output += "%s\n" % Table.make_row(
+                row,
+                col_widths,
+                column_colours=column_colours,
+                row_colour=row_colour
+            )
 
         return output
 
     @staticmethod
-    def make_row(row, col_widths, separator="|", has_key=False):
+    def make_row(row, col_widths, separator="|", column_colours=None, row_colour=None):
+        if column_colours is None:
+            column_colours = []
+        if row_colour is None:
+            row_colour = []
         string = Colour.light_grey(separator)
         for i, column in enumerate(row):
             data = str(column)
-            if i is 0 and has_key:
-                data = Colour.green(data)
+            colour = column_colours[i] if i < len(column_colours) else row_colour
+            if callable(colour):
+                data = colour(data)
             padding = "".join([" " for _ in range(col_widths[i] - len(str(column)) + 2)])
             string += "  " + data + padding + Colour.light_grey(separator)
 
@@ -36,3 +58,39 @@ class Table:
             underline += "".join(["-" for _ in range(width + 4)]) + "|"
 
         return Colour.light_grey(underline)
+
+
+if __name__ == '__main__':
+    plain = Table.create([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ['a', 'b', 'c'])
+    print(plain)
+    colouredRows = Table.create(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        ['a', 'b', 'c'],
+        row_colours=[Colour.red, Colour.blue]
+    )
+    print(colouredRows)
+    colouredCols = Table.create(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        ['a', 'b', 'c'],
+        column_colours=[Colour.red, Colour.blue, Colour.green]
+    )
+    print(colouredCols)
+    colouredRowsAndCols = Table.create(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        ['a', 'b', 'c'],
+        row_colours=[Colour.red, Colour.blue],
+        column_colours=[Colour.green]
+    )
+    print(colouredRowsAndCols)
+    colouredHeaders = Table.create(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        ['a', 'b', 'c'],
+        header_colours=[Colour.red, Colour.yellow, Colour.red],
+    )
+    print(colouredHeaders)
+    colouredHeader = Table.create(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        ['a', 'b', 'c'],
+        header_colour=Colour.cyan,
+    )
+    print(colouredHeader)
