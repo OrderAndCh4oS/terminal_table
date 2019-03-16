78132008396c89d806f85744fe8418f5d949f2a2
sarcoma <sarcoma@live.co.uk>
2019-03-16 10:01:51 +0000

    Add use_ansi flag to toggle off ansi colours

--- terminal_table/terminal_table.py
+++ terminal_table/terminal_table.py
@@ -3,7 +3,7 @@ from ansi_colours import AnsiColours as Colour
 
 class Table:
     @staticmethod
-    def create(rows, headers, row_colours=None, column_colours=None, header_colours=None, header_colour=None):
+    def create(rows, headers, use_ansi=True, row_colours=None, column_colours=None, header_colours=None, header_colour=None):
         if row_colours is None:
             row_colours = []
         if column_colours is None:
@@ -19,15 +19,17 @@ class Table:
         output += "%s\n" % Table.make_row(
             headers,
             col_widths,
+            use_ansi,
             column_colours=header_colours,
             row_colour=header_colour
         )
-        output += "%s\n" % Table.make_underline(col_widths)
+        output += "%s\n" % Table.make_underline(col_widths, use_ansi)
         for i, row in enumerate(rows):
             row_colour = row_colours[i % len(row_colours)] if len(row_colours) else None
             output += "%s\n" % Table.make_row(
                 row,
                 col_widths,
+                use_ansi,
                 column_colours=column_colours,
                 row_colour=row_colour
             )
@@ -35,33 +37,35 @@ class Table:
         return output
 
     @staticmethod
-    def make_row(row, col_widths, separator="|", column_colours=None, row_colour=None):
+    def make_row(row, col_widths, use_ansi=True, separator="|", column_colours=None, row_colour=None):
         if column_colours is None:
             column_colours = []
         if row_colour is None:
             row_colour = []
-        string = Colour.light_grey(separator)
+        separator = Colour.light_grey(separator) if use_ansi else separator
+        string = separator
         for i, column in enumerate(row):
             data = str(column)
-            colour = column_colours[i] if i < len(column_colours) else row_colour
-            if callable(colour):
-                data = colour(data)
+            if use_ansi:
+                colour = column_colours[i] if i < len(column_colours) else row_colour
+                if callable(colour):
+                    data = colour(data)
             padding = "".join([" " for _ in range(col_widths[i] - len(str(column)) + 2)])
-            string += "  " + data + padding + Colour.light_grey(separator)
+            string += "  " + data + padding + separator
 
         return string
 
     @staticmethod
-    def make_underline(col_widths, separator="|"):
+    def make_underline(col_widths, use_ansi=True, separator="|"):
         underline = separator
         for width in col_widths:
             underline += "".join(["-" for _ in range(width + 4)]) + "|"
 
-        return Colour.light_grey(underline)
+        return Colour.light_grey(underline) if use_ansi else underline
 
 
 if __name__ == '__main__':
-    plain = Table.create([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ['a', 'b', 'c'])
+    plain = Table.create([[1, 2, 3], [4, 5, 6], [7, 8, 9]], ['a', 'b', 'c'], use_ansi=False)
     print(plain)
     colouredRows = Table.create(
         [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
